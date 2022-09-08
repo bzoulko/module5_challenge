b3f7a27049d948ca85dc991c9e39540193468dce
BrianZoulko <bzoulko@gmail.com>
2022-09-08 14:36:21 -0700

    Finished the fully functioning scheduler.

--- assest/js/script.js
+++ assest/js/script.js
@@ -10,13 +10,14 @@
                              user to save events for each hour of the day in local storage.
 */
 
-
 // Declare container varaible that points to the web page container section.
 var cntSched = $('.container-fluid');
 
 // Create the appropriate number of time slots.
 var today = moment();
-var hour = today.format("h");
+var hour = today.format("H");
+var date = today.format('dddd, MMMM Do')
+var currentDay = $('#currentDay').text(date);
 
 // Format time elements from 9AM to 5PM and append to scheduler container.
 for (var x = 9; x < 18; x++) {
@@ -26,23 +27,22 @@ for (var x = 9; x < 18; x++) {
 }
 
 
-/* ****************************************
-  Add time slots to the work day scheduler.
-******************************************* */
-function appendTimeSlot (sched, time, addIt) {  
-
-  // var currentTime = 
-  // var additionalClass = 
-  // //window.document.preventDefault();
+/* *********************************************************************
+  Add time slots to the work day scheduler and preload and saved events.
+************************************************************************ */
+function appendTimeSlot (sched, time, addClass) {  
 
   // Declare each part of the day planner schedular line.
-  var frmSect  = $('<form class="form-inline">');
+  var key = time + ":" + date.split(",")[0];
+  var frmSect  = $('<form class="form-inline"> method="POST" onsubmit="false"');
   var divTime  = $('<div class="hour">');
-  var inpInput = $(`<input id=${time} class="form-control description col-xl col-lg-9 ${addIt}" type="text" aria-label="Work day Event">`); 
-  var btnSave  = $(`<button id=${time} class="btn saveBtn" type="button">`);
+  var inpInput = $(`<input id=${key} class="form-control description col-xl col-lg-9 ${addClass}" type="text" aria-label="Work day Event">`); 
+  var btnSave  = $(`<button id=${key} class="btn saveBtn" type="button">`);
   var imgIcon  = $('<img class="image-icon" src="./images/save.png">');
+  var data = getData(key);
 
   // Set time and append all elements into the schedular container.
+  inpInput.val(data);
   divTime.text(time);
   btnSave.append(imgIcon);
   frmSect.append(divTime);
@@ -50,25 +50,27 @@ function appendTimeSlot (sched, time, addIt) {
   frmSect.append(btnSave);
   sched.append(frmSect);
 
+
+  // On Button Click event save to local storage.
+  btnSave.on('click', function () {
+    putData(this.id, inpInput.val());    
+  });
+
+
+  /* ****************************************************************************
+    Store data in localStorage based on a key and the value or data being passed.
+  ******************************************************************************* */
+  function putData(key, value){
+    localStorage.setItem(key, value);
+  }
+
+
+  /* ****************************************************
+    Return the value for a specific key in local storage.
+  ******************************************************* */
+  function getData(key) {
+    console.log("getting data by key: " + key);
+    return(localStorage.getItem(key));
+  }
+  
 };
-
-// // Attach event listener to document to listen for key event
-// document.addEventListener("keydown", function(event) {
-//   // If the count is zero, exit function
-//   if (timerCount === 0) {
-//     return;
-//   }
-//   // Convert all keys to lower case
-//   var key = event.key.toLowerCase();
-//   var alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
-//   // Test if key pushed is letter
-//   if (alphabetNumericCharacters.includes(key)) {
-//     var letterGuessed = event.key;
-//     checkLetters(letterGuessed)
-//     checkWin();
-//   }
-// });
-
-// // Attach event listener to start button to call startGame function on click
-// startButton.addEventListener("click", startGame);
-
